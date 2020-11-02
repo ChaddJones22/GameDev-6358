aca55abe0ec0fb574e6a45ad17328fa404a054bf
Songwen <owensit98@gmail.com>
2020-11-02 14:36:12 -0600

    Added door

---
+++ Protal maybe/Assets/Scripts/Event_Door_Opener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_Door_Opener : BaseEvent
+{
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void ActivateEvent()
+    {
+        Debug.Log("Door Opened");
+        GameObject spawned = Instantiate(door, this.transform.position, transform.rotation);
+
+    }
+}
