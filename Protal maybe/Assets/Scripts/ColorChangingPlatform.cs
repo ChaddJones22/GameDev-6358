e90e4b6feca11a2433d380acb956ee1cf759e54c
ChaddJones22 <40244021+ChaddJones22@users.noreply.github.com>
2020-11-10 02:41:17 -0600

    Fix walking++
    Added friction to walking to walk up slopes.
    Made exploding walls, falling platform, invis platforms, and falling walls react to bullet impact.
    Updated some platform prefabs.
    Created exploding door partial system prefabs.
    Created color changing platforms .

---
+++ Protal maybe/Assets/Scripts/ColorChangingPlatform.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangingPlatform : MonoBehaviour
+{
+    public float waitTime = 0.01f;
+    public Color hitColor;
+    public Color returnColor;
+
+    bool ifRed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ifRed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //destorys door with bullet hit
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            GetComponent<SpriteRenderer>().color = hitColor;
+            ifRed = true;
+            waitTime = waitTime;
+
+        }
+        //calls to fade back
+        if (ifRed)
+        {
+            StartCoroutine("FadeBack");
+        }
+    }
+
+    //Fades back to starting color
+    IEnumerator FadeBack()
+        {
+            yield return new WaitForSeconds(waitTime);
+            GetComponent<SpriteRenderer>().color = returnColor;
+        }
+
+}
