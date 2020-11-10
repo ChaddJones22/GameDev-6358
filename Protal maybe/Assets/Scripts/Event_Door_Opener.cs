4b38680374d2ceb6fbadb026f20f9235b81aecdb
Songwen <owensit98@gmail.com>
2020-11-09 23:45:20 -0600

    updated door

--- Protal maybe/Assets/Scripts/Event_Door_Opener.cs
+++ Protal maybe/Assets/Scripts/Event_Door_Opener.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Event_Door_Opener : BaseEvent
 {
     public GameObject door;
+    public Transform pivot;
+    public BoxCollider2D doorSelect;
+    public float speedModifier;
+    bool door_activate = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +20,21 @@ public class Event_Door_Opener : BaseEvent
     // Update is called once per frame
     void Update()
     {
-
+        if (door_activate == true){
+            door.transform.localScale = new Vector3(0,0,0);
+        }
     }
 
     public override void ActivateEvent()
     {
         Debug.Log("Door Opened");
-        GameObject spawned = Instantiate(door, this.transform.position, transform.rotation);
 
+        doorSelect.enabled=false;
+
+        door_activate = true;
     }
+
+
 }
+
+
