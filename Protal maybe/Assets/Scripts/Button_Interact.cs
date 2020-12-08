ca90a970ff873fb11909e0f38b63c7756ee54450
Phuoc Tran <55820561+zeroliger@users.noreply.github.com>
2020-12-07 23:30:26 -0600

    New Stage for 7

--- Protal maybe/Assets/Scripts/Button_Interact.cs
+++ Protal maybe/Assets/Scripts/Button_Interact.cs
@@ -9,6 +9,9 @@ public class Button_Interact : MonoBehaviour
     public bool PlayerInteractable;
     public bool continuos;
     public bool NotNormal;
+
+    public bool multiEvent;
+    public GameObject[] ConnectedMultiEvents;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +26,23 @@ public class Button_Interact : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        
+            
         if ((collision.gameObject.tag == "Bullet" || collision.gameObject.tag=="Box") && !NotNormal)
         {
             Debug.Log("Contact");
-            ConnectedEvent.GetComponent<BaseEvent>().ActivateEvent();
+            if (multiEvent)
+            {
+                for (int x = 0; x < ConnectedMultiEvents.Length; x++)
+                {
+                    ConnectedMultiEvents[x].GetComponent<BaseEvent>().ActivateEvent();
+                }
+            }
+            else
+            {
+                ConnectedEvent.GetComponent<BaseEvent>().ActivateEvent();
+            }
+                
         }
         else if(collision.gameObject.tag == "Box" && NotNormal)
         {
@@ -36,16 +52,58 @@ public class Button_Interact : MonoBehaviour
 
         if(PlayerInteractable && collision.gameObject.tag=="Player")
         {
-            ConnectedEvent.GetComponent<BaseEvent>().ActivateEvent();
+            if (multiEvent)
+            {
+                for (int x = 0; x < ConnectedMultiEvents.Length; x++)
+                {
+                    ConnectedMultiEvents[x].GetComponent<BaseEvent>().ActivateEvent();
+                }
+            }
+            else
+            {
+                ConnectedEvent.GetComponent<BaseEvent>().ActivateEvent();
+            }
         }
             
     }
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (continuos)
+        {
+            if (multiEvent)
+            {
+                for (int x = 0; x < ConnectedMultiEvents.Length; x++)
+                {
+                    ConnectedMultiEvents[x].GetComponent<BaseEvent>().ActivateEvent();
+                }
+            }
+            else
+            {
+                ConnectedEvent.GetComponent<BaseEvent>().ActivateEvent();
+            }
+
+        }
+
+    }
+
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if(continuos)
+        if (continuos)
         {
-            ConnectedEvent.GetComponent<BaseEvent>().EndEvent();
+            if (multiEvent)
+            {
+                for (int x = 0; x < ConnectedMultiEvents.Length; x++)
+                {
+                    ConnectedMultiEvents[x].GetComponent<BaseEvent>().EndEvent();
+                }
+            }
+            else
+            {
+                ConnectedEvent.GetComponent<BaseEvent>().EndEvent();
+            }
+           
         }
         
     }
