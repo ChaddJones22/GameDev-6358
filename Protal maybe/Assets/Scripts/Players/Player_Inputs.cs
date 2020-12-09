c563e307c8868e48d9972fd0e3cc04247ac244df
esherna7 <60415041+esherna7@users.noreply.github.com>
2020-12-09 14:46:56 -0600

    Added pause menu system
    added pause menu via canvas to each stage. made as prefab for ease of use but eventsystem object needed in stage to work. updated player_inputs script to stop animations and actions while paused

--- Protal maybe/Assets/Scripts/Players/Player_Inputs.cs
+++ Protal maybe/Assets/Scripts/Players/Player_Inputs.cs
@@ -29,89 +29,95 @@ public class Player_Inputs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Checks if weapon swap was done for gameplay mechnaic of avoiding shooting CD
-        if (equipNum != storedEquip)
-        {
-            checkSwap = true;
-        }
-        storedEquip = equipNum;
-        //Checks for horizontal inputs
-        xInput = Input.GetAxis("Horizontal");
 
-        //Controls Jumping
-        if(Input.GetKeyDown(KeyCode.Space))
+        //continue actions and animations as long as game is not paused
+        if (!Pause_Menu.GameIsPaused)
         {
-            jumpInput = true;
-        }
-        else
-        {
-            jumpInput = false;
-        }
 
 
-        //Controls for which gun is shooting 1 and 2 is only when click 3 is hold 
-        switch (equipNum)
-        {
-            default:
-                {
-                    if (Input.GetMouseButtonDown(0) && !CoolDown)
+            //Checks if weapon swap was done for gameplay mechnaic of avoiding shooting CD
+            if (equipNum != storedEquip)
+            {
+                checkSwap = true;
+            }
+            storedEquip = equipNum;
+            //Checks for horizontal inputs
+            xInput = Input.GetAxis("Horizontal");
+
+            //Controls Jumping
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumpInput = true;
+            }
+            else
+            {
+                jumpInput = false;
+            }
+
+
+            //Controls for which gun is shooting 1 and 2 is only when click 3 is hold 
+            switch (equipNum)
+            {
+                default:
                     {
-                        shoot = true;
+                        if (Input.GetMouseButtonDown(0) && !CoolDown)
+                        {
+                            shoot = true;
 
-                        Audio.clip = gunSounds[equipNum-1];
-                        Audio.Play();
+                            Audio.clip = gunSounds[equipNum - 1];
+                            Audio.Play();
 
+                        }
+                        break;
                     }
-                    break;
-                }
-           
-            case 3:
-                {
-                    if (Input.GetMouseButton(0)&& !CoolDown)
+
+                case 3:
                     {
-                        shoot = true;
+                        if (Input.GetMouseButton(0) && !CoolDown)
+                        {
+                            shoot = true;
 
-                        //controls which audio clip for gun shot
-                        Audio.clip = gunSounds[2];
-                        Audio.Play();
+                            //controls which audio clip for gun shot
+                            Audio.clip = gunSounds[2];
+                            Audio.Play();
+                        }
+                        break;
                     }
-                    break;
-                }
 
-            case 0:
-                {
-                    break;
-                }
-        }
+                case 0:
+                    {
+                        break;
+                    }
+            }
 
 
-        //Controls to switch to proper gun on numkey press
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            equipNum = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            equipNum = 2;
-        }
-        if(Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            equipNum = 3;
-        }
+            //Controls to switch to proper gun on numkey press
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                equipNum = 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                equipNum = 2;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                equipNum = 3;
+            }
 
 
-        //Tracks Crosshair to mouse position
-        crosshair.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
-        crosshair.transform.position = new Vector3(crosshair.transform.position.x, crosshair.transform.position.y, 0);
-        if(crosshair.transform.localPosition.y<-4.3f)
-        {
-            cam.GetComponent<Camera_Movement>().lookDown = true;
+            //Tracks Crosshair to mouse position
+            crosshair.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+            crosshair.transform.position = new Vector3(crosshair.transform.position.x, crosshair.transform.position.y, 0);
+            if (crosshair.transform.localPosition.y < -4.3f)
+            {
+                cam.GetComponent<Camera_Movement>().lookDown = true;
+            }
+            else if (crosshair.transform.localPosition.y > -1f)
+            {
+                cam.GetComponent<Camera_Movement>().lookDown = false;
+            }
         }
-        else if(crosshair.transform.localPosition.y > -1f)
-        {
-            cam.GetComponent<Camera_Movement>().lookDown = false;
-        }
-
     }
     
 }
